35b93d40d6f641ecaef4a30c875202a9fd514867
Artem Safin <artemsafin.webdev@gmail.com>
2026-04-14 14:18:36 +0300

    По странице

---
+++ pages-data/super-format.js
@@ -0,0 +1,20 @@
+export default {
+  "/super-format.html": {
+    title: "Суперформат",
+    hero: {
+      titleHtml: "Премиальные<br>суперформаты<br>для лидеров рынка",
+      descriptionHtml:
+        "Размещайте рекламу на&nbsp;премиум-медиафасадах в&nbsp;топовых локациях&nbsp;&mdash; усиливайте имидж в&nbsp;масштабах страны!",
+      buttonText: "Оставить заявку",
+      image: {
+        desktopSrc: "/images/dom-lenta-super-format-hero/desktop.webp",
+        mobileSrc: "/images/dom-lenta-super-format-hero/mobile.webp",
+        alt: "",
+      },
+      logo: {
+        src: "/images/dom-lenta-super-format-hero/logo.svg",
+        alt: "Суперформат.РФ",
+      },
+    },
+  },
+};
